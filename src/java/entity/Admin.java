1c2201d07fc5c69f06f811ef1c3259d657dc466d
Val Notman <val.notman.cms@gmail.com>
2020-05-09 23:14:29 -0500

    Unusable path.

---
+++ src/java/entity/Admin.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Kaijing
+ */
+@Entity
+public class Admin extends User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Admin() {
+    }
+
+    public Admin(String username, String password, String firstName, String lastName, String email) {
+        super(username, password, firstName, lastName, email);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userId != null ? userId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Admin)) {
+            return false;
+        }
+        Admin other = (Admin) object;
+        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Admin[ id=" + userId + " ]";
+    }
+    
+}
